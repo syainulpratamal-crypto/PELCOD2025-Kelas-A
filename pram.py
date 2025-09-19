ec553d8f1dee4dce4a72ed94ed4cbe91100a9bef
syainulpratamal-crypto <syainulpratamal@gmail.com>
2025-09-19 22:02:13 +0700

    250441100004

---
+++ pram.py
@@ -0,0 +1,43 @@
+print("="*50)
+print("nama saya pram")
+print("="*50)
+nama = "pram ganteng"
+print (nama)
+print (type(nama))
+boolean = True
+print(type(boolean))
+
+penjumlahan = 5+7
+print("hasilnya adalah",penjumlahan)
+
+nama_asprak = "pram kamal"
+nama_calon_dosen = nama_asprak
+print(nama_calon_dosen)
+
+
+# tipe data number
+
+# tahun_lahir = int(input("masukkan tahun lahir : "))
+# tahun_sekarang = int(input("masukkan tahun sekarang : "))
+# penghitungan_usia = tahun_sekarang - tahun_lahir
+# print(penghitungan_usia)
+
+bilangan_1 = 51
+bilangan_2 = 7
+modulus = bilangan_1 % bilangan_2
+print(modulus)
+# jumlah = int(bilangan)+int(bilangan_2)
+# print(jumlah)
+
+# cek = bilangan > bilangan_2
+# print(cek)
+
+saldo = float(input("masukan saldo awal : "))
+deposit = float(input("masukan saldo deposit : "))
+
+saldo += saldo 
+print(saldo)
+print
+
+
+
